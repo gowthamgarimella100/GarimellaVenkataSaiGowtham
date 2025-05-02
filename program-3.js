0db91837bd4ef86d0a88788ec881af7f4f7a847e
Gowtham Garimella <146919013+gowthamgarimella100@users.noreply.github.com>
2025-05-02 16:03:04 +0530

    Create program-3.js

---
+++ program-3.js
@@ -0,0 +1,12 @@
+const input_value = 11;
+generateOddNumberSeries(input_value);
+function generateOddNumberSeries(value){
+  let result = [];
+  const x = value%2===0?value-1:value
+  let y = 1;
+  for(let i=0;i<x;i++){
+    resilt.push(y);
+    y+=2
+  }
+  return result.join(',');
+}
